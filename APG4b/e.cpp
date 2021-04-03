6b7799ada93b083c9b13bbf2d0dc291e06c7f468
takuroyamasaki <cobain6339@gmail.com>
2021-04-04 02:01:04 +0900

    //

---
+++ APG4b/e.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // 一年の秒数
+    int seconds = 365 * 24 * 60 * 60;
+
+    cout << 1 * seconds/* 1年は何秒か */ << endl;
+    cout << 2 * seconds/* 2年は何秒か */ << endl;
+    cout << 5 * seconds/* 5年は何秒か */ << endl;
+    cout << 10 * seconds/* 10年は何秒か */ << endl;
+}
