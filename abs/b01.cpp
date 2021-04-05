cd6c546e7fc8c385b7fdbfe234f0de1fc3494f79
takuroyamasaki <cobain6339@gmail.com>
2021-04-06 00:52:10 +0900

    AtCoder Beginners Selection

--- abs/b01.cpp
+++ abs/b01.cpp
@@ -3,17 +3,34 @@
 using namespace std;
 
 int main() {
-  vector<char> vec(3);
-  for (int i = 0; i < 3; i++) {
+  int N;
+  cin >> N;
+  vector<int> vec(N);
+  for (int i = 0; i < N; i++) {
     cin >> vec.at(i);
   }
-
+  //奇数フラグ
+  bool isOdd = false;
+  //計算回数
   int result = 0;
-  for (int i = 0; i < 3; i++) {
-    if (vec.at(i) == '1') {
+  while (1) {
+    for (int i = 0; i < N; i++) {
+      if ((vec.at(i) % 2) == 0) {
+        vec.at(i) = vec.at(i) / 2;
+      } else {
+        //奇数が見つかった場合は中断してループを抜ける
+        isOdd = true;
+        break;
+      }
+    }
+    //奇数フラグがtrueの場合ループを抜けて終了
+    if (isOdd) {
+      break;
+    } else {
+      //奇数フラグがfalseなので計算回数を加算してループ継続
       result++;
     }
   }
 
-  cout << result;
+  cout << result << endl;
 }
