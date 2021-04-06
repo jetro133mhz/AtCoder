3a1d2adacc262276a41860351bbe7c615a574ee9
takuroyamasaki <cobain6339@gmail.com>
2021-04-07 00:01:18 +0900

    Programming Guide for beginners

---
+++ APG4b/r.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  bool result = false;
+
+  for (int i = 0; i < 4; i++) {
+    if (data.at(i) == data.at(i + 1)) {
+      result = true;
+    }
+  }
+
+  if (result) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
