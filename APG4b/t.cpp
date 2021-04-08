c10f405bb032722f685ce52b2e9fb2510fafb061
takuroyamasaki <cobain6339@gmail.com>
2021-04-08 22:29:34 +0900

    Programming Guide for beginners

--- APG4b/t.cpp
+++ APG4b/t.cpp
@@ -3,31 +3,37 @@
 using namespace std;
 
 int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M), B(M);
+  for (int i = 0; i < M; i++) {
+    cin >> A.at(i) >> B.at(i);
+  }
 
-  // int型の2次元配列(3×4要素の)の宣言
-  vector<vector<int>> data(3, vector<int>(4));
-
-  // 入力 (2重ループを用いる)
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-      cin >> data.at(i).at(j);
+  // 結果格納用配列定義・初期化
+  vector<vector<char>> R(N, vector<char>(N));
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      R.at(i).at(j) = '-';
     }
   }
 
-  // 0の個数を数える
-  int count = 0;
+  //結果格納
+  for (int i = 0; i < M; ++i) {
+    int a = A.at(i) - 1;
+    int b = B.at(i) - 1;
+    R.at(a).at(b) = 'o';
+    R.at(b).at(a) = 'x';
+  }
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-
-      // 上からi番目、左からj番目の画素が0かを判定
-      if (data.at(i).at(j) == 0) {
-        count++;
+  //結果を出力
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      cout << R.at(i).at(j);
+      if (j != (N - 1)) {
+        cout << ' ';
       }
-
     }
+    cout << endl;
   }
-
-  cout << count << endl;
-
 }
