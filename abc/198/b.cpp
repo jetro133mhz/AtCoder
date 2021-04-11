293b0ace15ecb97baadda77f037c69e934700ea4
takuroyamasaki <cobain6339@gmail.com>
2021-04-12 00:12:25 +0900

    Beginner Contest 198

--- abc/198/b.cpp
+++ abc/198/b.cpp
@@ -6,23 +6,20 @@ using namespace std;
 int main() {
   string N1, N2;
   cin >> N1;
-  int count = N1.size();
+
   bool result = false;
-  for (int i = 0; i < count; ++i) {
+  //入力の値の範囲から10までで問題なし
+  for (int i = 0; i < 10; ++i) {
     N2 = N1;
     reverse(N2.begin(), N2.end());
+    //元の文字列と、リバースにした文字列を比較して一致すればフラグを立ててループを抜ける
     if (N1 == N2) {
       cout << "Yes" << endl;
       result = true;
       break;
     } else {
+      //一致しない場合は、N1の先頭に0を追加してループ継続
       N1.insert(0, "0");
-      reverse(N2.begin(), N2.end());
-      if (N1 == N2) {
-        cout << "Yes" << endl;
-        result = true;
-        break;
-      }
     }
   }
   if (!result) {
