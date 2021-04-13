36bd250bea7acb3f0d4c834c81c2ca7134d17ebf
takuroyamasaki <cobain6339@gmail.com>
2021-04-14 01:06:29 +0900

    AOJ

---
+++ aoj/ITP1_2_C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  vector<int> num(3);
+  for (int i = 0; i < 3; ++i) {
+    cin >> num.at(i);
+  }
+  sort(num.begin(), num.end());
+  for (int i = 0; i < 3; ++i) {
+    cout << num.at(i);
+    if (i != 2) {
+      cout << " ";
+    }
+  }
+  cout << endl;
+}
