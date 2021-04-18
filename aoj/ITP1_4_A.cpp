5628b85917207e6ae85c5bd95842836abbd598cc
takuroyamasaki <cobain6339@gmail.com>
2021-04-19 00:19:09 +0900

    AOJ

---
+++ aoj/ITP1_4_A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+  cout << (int) a / (int) b << " " << (int) a % (int) b << " " << setprecision(5) << a / b << endl;
+}
