e2eaaa19deaf063d6d3448f892ffbb62e01fea5e
takuroyamasaki <cobain6339@gmail.com>
2021-04-20 01:11:04 +0900

    AOJ

---
+++ aoj/ITP1_4_D.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  long long int n;
+  cin >> n;
+  long long int sum = 0;
+  vector<int> vec(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> vec.at(i);
+    sum += vec.at(i);
+  }
+  sort(vec.begin(), vec.end());
+  cout << vec.at(0) << " " << vec.at(n - 1) << " " << sum << endl;
+}
