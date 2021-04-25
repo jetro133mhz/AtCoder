68e969b1423bd5aac48d58e940b7815a2fcaea9a
takuroyamasaki <cobain6339@gmail.com>
2021-04-25 14:13:36 +0900

    Beginner Contest 199

--- abc/199/c.cpp
+++ abc/199/c.cpp
@@ -13,20 +13,8 @@ int main() {
     if (t == 1) {
       swap(s.at(a - 1), s.at(b - 1));
     } else {
-//      vector<char> firstHalf(n);
-//      vector<char> latterHalf(n);
-//      for (int j = 0; j < n; ++j) {
-//        firstHalf.at(j) = s.at(j);
-//        latterHalf.at(j) = s.at(n - j);
-//      }
-//      for (int j = 0; j < n; ++j) {
-//        s.at(j) = latterHalf.at(j);
-//      }
-//      for (int j = n; j < 0; --j) {
-//        s.at(j) = firstHalf.at(n - j);
-//      }
-
       //以下ではTLE
+      // t=2 の場合の対処が必要
       s = s.substr(2 * n - n, n) + s.substr(0, n);
     }
   }
