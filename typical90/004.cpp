f0bede6d77cfab5c7b04d5d73745934f046e19a1
takuroyamasaki <cobain6339@gmail.com>
2021-04-29 23:20:57 +0900

    競プロ典型 90 問

--- typical90/004.cpp
+++ typical90/004.cpp
@@ -3,26 +3,24 @@
 using namespace std;
 
 int main() {
-  //TLEになるので駄目
   int h, w;
   cin >> h >> w;
   vector<vector<int>> B(w, vector<int>(h));
+  vector<int> sum_h(h, 0);
+  vector<int> sum_w(w, 0);
   for (int i = 0; i < h; ++i) {
     for (int j = 0; j < w; ++j) {
       cin >> B.at(j).at(i);
+      //各行、各列の和を予め計算して格納しておく
+      sum_h.at(i) += B.at(j).at(i);
+      sum_w.at(j) += B.at(j).at(i);
     }
   }
 
   for (int i = 0; i < h; ++i) {
     for (int j = 0; j < w; ++j) {
       int sum = 0;
-      for (int k = 0; k < w; ++k) {
-        sum += B.at(k).at(i);
-      }
-      for (int k = 0; k < h; ++k) {
-        sum += B.at(j).at(k);
-      }
-      cout << sum - B.at(j).at(i);
+      cout <<  sum_h.at(i) + sum_w.at(j) - B.at(j).at(i);
       if (j != w - 1) {
         cout << " ";
       }
