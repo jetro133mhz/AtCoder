042f5b49c69ccfb2681e77f3612a1a0094198331
takuroyamasaki <cobain6339@gmail.com>
2021-04-29 23:05:37 +0900

    競プロ典型 90 問

---
+++ typical90/004.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<vector<int>> B(w, vector<int>(h));
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      cin >> B.at(j).at(i);
+    }
+  }
+
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      int sum = 0;
+      for (int k = 0; k < w; ++k) {
+        sum += B.at(k).at(i);
+      }
+      for (int k = 0; k < h; ++k) {
+        sum += B.at(j).at(k);
+      }
+      cout << sum - B.at(j).at(i);
+      if (j != w - 1) {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
