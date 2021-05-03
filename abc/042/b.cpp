b23ab69f9494ee66e55f2039430a7e56877392ca
takuroyamasaki <cobain6339@gmail.com>
2021-05-04 02:33:51 +0900

    ABC042

--- abc/042/b.cpp
+++ abc/042/b.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
   int n, l;
   cin >> n >> l;
+  //文字列を一旦配列に格納し、ソートした結果をそのまま出力すればいい
   vector<string> vec_s(n);
   for (int i = 0; i < n; ++i) {
     cin >> vec_s.at(i);
