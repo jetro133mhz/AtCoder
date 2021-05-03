b76e2550fa8efbe718f293c16656a40f6354ae23
takuroyamasaki <cobain6339@gmail.com>
2021-05-04 02:31:07 +0900

    ABC042

--- abc/042/a.cpp
+++ abc/042/a.cpp
@@ -5,30 +5,10 @@ using namespace std;
 int main() {
   int a, b, c;
   cin >> a >> b >> c;
-  int count_5 = 0;
-  int count_7 = 0;
-
-  if (a == 5) {
-    count_5++;
-  }
-  if (b == 5) {
-    count_5++;
-  }
-  if (c == 5) {
-    count_5++;
-  }
-
-  if (a == 7) {
-    count_7++;
-  }
-  if (b == 7) {
-    count_7++;
-  }
-  if (c == 7) {
-    count_7++;
-  }
-
-  if (count_7 == 1 && count_5 == 2) {
+  //abcを文字列に連結してソートした結果 557 となればYES
+  string s = to_string(a) + to_string(b) + to_string(c);
+  sort(s.begin(), s.end());
+  if (s == "557") {
     cout << "YES" << endl;
   } else {
     cout << "NO" << endl;
